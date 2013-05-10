3f2b55e94b5387e63181b2ad5d54e9f7db8c3266
Napalias <napalias@gmail.com>
2013-05-10 15:49:59 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html> 
+    <head> 
+ 
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
+ 
+        <title></title> 
+ 
+        <meta name="Publisher" content="" /> 
+        <meta name="description" content="" /> 
+        <meta name="keywords" content="" /> 
+        <meta name="Language" content="LT" /> 
+        <meta name="Robots" content="ALL" /> 
+ 
+        <meta name="Distribution" content="GLOBAL" /> 
+        <meta name="Identifier-URL" content="" /> 
+ 
+ 
+       
+        
+ 
+ 
+        <link href="css/style.css" rel="stylesheet" type="text/css" /> 
+ 
+
+
+ 
+    </head> 
+    <body> 
+	
+	
+	</body>
+</html>
